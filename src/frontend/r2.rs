9bf5cfca8ab11b3ddd1e5770eed4326968e6d3b7
Sushant <sushant.dinesh94@gmail.com>
2015-06-17 00:34:23 +0530

    Added better json parsing

--- src/frontend/r2.rs
+++ src/frontend/r2.rs
@@ -2,16 +2,62 @@
 //! Uses r2pipe.
 #![allow(dead_code)]
 use r2pipe::R2Pipe;
-use rustc_serialize::json::Json;
-
+use rustc_serialize::json::{Json, ToJson};
+use std::collections::BTreeMap;
 struct R2 {
     pipe: R2Pipe,
     readin: String,
 }
 
-struct JsonWrapper<T> {
-    json: Json,
-    data: T,
+pub trait Search {
+    fn get_values(&self, key: &[&str], deep: bool) -> Option<Json>;
+}
+
+impl Search for Json {
+    fn get_values(&self, key: &[&str], deep: bool) -> Option<Json> {
+        match *self {
+            Json::Array(ref arr) => {
+                let mut tmp = Vec::new();
+                for x in arr.iter() {
+                    x.get_values(key, deep)
+                     .map_or((), |e| tmp.push(e));
+                }
+                if tmp.len() > 0 {
+                    return Some(tmp.to_json());
+                }
+            },
+            
+            Json::Object(ref obj) => {
+                let mut result: BTreeMap<String, Json> = BTreeMap::new();
+                for k in key {
+                    obj.get((*k)).map_or((), |val| {
+                        result.insert((*k).to_string(), val.clone());
+                    });
+                }
+
+                // If we have found something, return.
+                if result.keys().len() > 0 {
+                    return Some(result.to_json());
+                }
+
+                // If we don't want a deep search, we just return None at this point.
+                if !deep {
+                    return None;
+                }
+
+                // Manually iterate through the keys and check if the values have the key.
+                for (_, val) in obj.iter() {
+                    let res = val.get_values(key, deep);
+                    if res.is_some() {
+                        return res;
+                    }
+                }
+            },
+            
+            _ => return None,
+        }
+        return None;
+    }
 }
 
 // fn send and recv allow users to send their own commands, i.e. The ones that are not currently
@@ -52,21 +98,26 @@ impl R2 {
         self.flush();
     }
 
+    fn build_type(&mut self) {
+    }
+
     pub fn get_fn_list(&mut self) -> Json {
         self.send("aflj");
         self.recv_json()
     }
-
 }
 
 mod test {
     use frontend::r2::R2;
+    use frontend::r2::Search;
 
     #[test]
     fn test1() {
-        let mut r2 = R2::new("/bin/ls");
+        let mut r2 = R2::new("./key");
         r2.analyze();
-        println!("{}", r2.get_fn_list().as_array().unwrap()[0].find_path(&["name"]).unwrap());
-        //println!("{}", r2.recv());
+        let fn_data = r2.get_fn_list();
+        assert_eq!(fn_data.get_values(&["addr"], false), None);
+        assert!(fn_data.get_values(&["addr"], true).is_some());
+        println!("{}", fn_data.get_values(&["name", "offset"], false).unwrap().pretty());
     }
 }
