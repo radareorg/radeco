bee00571abd4e20ea55f0fc049049a4a3ab886e7
David Kreuter <dkreuter@gmail.com>
2015-08-13 21:51:37 +0200

    verifier.rs: Replace panic!() by return Err()

--- src/middle/ssa/verifier.rs
+++ src/middle/ssa/verifier.rs
@@ -17,8 +17,8 @@ use super::ssastorage::SSAStorage;
 use middle::ir::{MArity, MOpcode};
 
 pub trait Verify: SSA {
-	fn verify_block(&self, i: &Self::ActionRef);
-	fn verify_expr(&self, i: &Self::ValueRef);
+	fn verify_block(&self, i: &Self::ActionRef) -> Result<(), SSAFault<Self>>;
+	fn verify_expr(&self, i: &Self::ValueRef) -> Result<(), SSAFault<Self>>;
 }
 
 pub trait VerifiedAdd: SSAMod {
@@ -37,15 +37,39 @@ impl<T: Verify + SSAMod> VerifiedAdd for T {
 	}
 }
 
+#[derive(Debug)]
+pub enum AnyRef<T: SSA> {
+	Value(T::ValueRef),
+	Action(T::ActionRef),
+	None
+}
+
+#[derive(Debug)]
+pub struct SSAFault<T: SSA> {
+	pub node: AnyRef<T>,
+	pub message: Option<String>,
+}
+
+macro_rules! check {
+	($ri: expr, $cond:expr, $message:expr) => (
+		if !$cond { return Err(SSAFault{node: $ri, message: Some($message.to_owned())}) }
+	);
+	($ri: expr, $cond:expr) => (
+		if !$cond { return Err(SSAFault{node: $ri, message: None}) }
+	)
+}
+
 impl Verify for SSAStorage {
-	fn verify_block(&self, block: &NodeIndex) {
+	fn verify_block(&self, block: &NodeIndex) -> Result<(), SSAFault<Self>> {
+		let ri = AnyRef::Action(*block);
+
 		let edge_count = self.edge_count();
 		let node_count = self.node_count();
 		// Make sure that we have a valid node first.
 		assert!(block.index() < node_count);
 		let edges = self.edges_of(block);
 		// Every BB can have a maximum of 2 Outgoing CFG Edges.
-		assert!(edges.len() < 3);
+		check!(ri, edges.len() < 3);
 
 		let mut edgecases = [false; 256];
 
@@ -53,8 +77,8 @@ impl Verify for SSAStorage {
 			match self.g[*edge] {
 				EdgeData::Control(i) => {
 					let target = self.target_of(edge);
-					assert!(self.is_action(target));
-					assert!(!edgecases[i as usize]);
+					check!(ri, self.is_action(target));
+					check!(ri, !edgecases[i as usize]);
 					edgecases[i as usize] = true;
 				},
 				_ => ()
@@ -69,7 +93,7 @@ impl Verify for SSAStorage {
 					//  * There must be a minimum of two edges.
 					//  * There _must_ be a selector.
 					//  * The jump targets must not be the same block.
-					assert!(edges.len() == 2);
+					check!(ri, edges.len() == 2);
 					let other_edge = match i {
 						0 => self.true_edge_of(block),
 						1 => self.false_edge_of(block),
@@ -77,7 +101,7 @@ impl Verify for SSAStorage {
 					};
 					let target_1 = self.target_of(edge);
 					let target_2 = self.target_of(&other_edge);
-					assert!(target_1 != target_2);
+					check!(ri, target_1 != target_2);
 					// No need to test the next edge.
 					break;
 				},
@@ -88,33 +112,36 @@ impl Verify for SSAStorage {
 					//  * Make sure we have not introduced an unconditional jump
 					//    which self-loops.
 					let target_block = self.target_of(edge);
-					assert!(edges.len() == 1);
-					assert!(self.selector_of(block).is_none());
-					assert!(target_block.index() < node_count);
+					check!(ri, edges.len() == 1);
+					check!(ri, self.selector_of(block).is_none());
+					check!(ri, target_block.index() < node_count);
 					let valid_block = if let NodeData::BasicBlock(_) = self.g[target_block] {
 						true
 					} else {
 						false
 					};
-					//assert!(valid_block);
-					assert!(*block != target_block);
+					//check!(ri, valid_block);
+					check!(ri, *block != target_block);
 				},
 				_ => panic!("Found something other than a control edge!"),
 			}
 		}
 
 		if edges.len() > 2 {
-			assert!(self.selector_of(block).is_none());
+			check!(ri, self.selector_of(block).is_none());
 		} else if edges.len() > 1{
-			assert!(self.selector_of(block).is_some());
+			check!(ri, self.selector_of(block).is_some());
 		}
 
 		// Make sure that this block is reachable.
 		let incoming = self.incoming_edges(block);
-		assert!((incoming.len() > 0) || *block == self.start_node());
+		check!(ri, (incoming.len() > 0) || *block == self.start_node());
+		Ok(())
 	}
 
-	fn verify_expr(&self, i: &NodeIndex) {
+	fn verify_expr(&self, i: &NodeIndex) -> Result<(), SSAFault<Self>> {
+		let ri = AnyRef::Value(*i);
+
 		let node_count = self.node_count();
 		// Make sure we have a valid node first.
 		assert!(i.index() < node_count);
@@ -143,56 +170,58 @@ impl Verify for SSAStorage {
 
 				{
 					let panic_str = format!("Expression {:?} has {} operands", opcode, op_len);
-					assert!(op_len == n, panic_str);
+					check!(ri, op_len == n, panic_str);
 				}
 
-				if n == 0 { return; }
+				if n == 0 { return Ok(()) }
 				match opcode {
 					MOpcode::OpNarrow(w0) => {
 						let w0 = self.get_node_data(&operands[0])
 						             .map(&extract)
 						             .unwrap();
-						assert!(w0 > w);
+						check!(ri, w0 > w);
 					},
 				    MOpcode::OpWiden(w0) =>  {
 						let w0 = self.get_node_data(&operands[0])
 						             .map(&extract)
 						             .unwrap();
-						assert!(w0 < w);
+						check!(ri, w0 < w);
 					},
 					MOpcode::OpCmp | MOpcode::OpGt | MOpcode::OpLt | MOpcode::OpLteq | MOpcode::OpGteq => {
 						let panic_str = format!("Expected width to be 1, found: {}", w);
-						assert!(w == 1, panic_str);
+						check!(ri, w == 1, panic_str);
 					},
 					_ => {
 						let w0 = self.get_node_data(&operands[0])
 						             .map(&extract)
 						             .unwrap();
-						assert!(w0 == w, format!("{:?} == {:?}; {:?}", w0, w, opcode));
+						check!(ri, w0 == w, format!("{:?} == {:?}; {:?}", w0, w, opcode));
 						for op in operands.iter() {
 							let w1 = self.get_node_data(op)
 							             .map(&extract)
 						                 .unwrap();
 							let panic_str = format!("{:?}: Expected size to be: {}, found: {}", opcode, w, w1);
-							assert!(w1 == w, panic_str);
+							check!(ri, w1 == w, panic_str);
 						}
 					},
 				}
 			},
-			_ => panic!("Found something other than an expression!"),
+			_ => check!(ri, false, "Found something other than an expression!"),
 		}
+		Ok(())
 	}
 }
 
-pub fn verify<T>(ssa: &T) where T: Verify {
+pub fn verify<T>(ssa: &T) -> Result<(), SSAFault<T>> where T: Verify {
 	let blocks = ssa.blocks();
 	for block in blocks.iter() {
 		// assert the qualities of the block first.
-		ssa.verify_block(block);
+		ssa.verify_block(block).ok().unwrap();
 		// Iterate through each node in the block and assert their properties.
 		let exprs = ssa.exprs_in(block);
 		for expr in exprs.iter() {
-			ssa.verify_expr(expr);
+			try!(ssa.verify_expr(expr));
 		}
 	}
+	Ok(())
 }
