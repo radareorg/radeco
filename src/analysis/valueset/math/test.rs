32bbc0a800093b2e9e57fa8f0184e836fcfc67df
Sushant <sushant.dinesh94@gmail.com>
2015-09-08 01:19:52 +0530

    Adds License header to files

--- src/analysis/valueset/math/test.rs
+++ src/analysis/valueset/math/test.rs
@@ -1,3 +1,10 @@
+// Copyright (c) 2015, The Radare Project. All rights reserved.
+// See the COPYING file at the top-level directory of this distribution.
+// Licensed under the BSD 3-Clause License:
+// <http://opensource.org/licenses/BSD-3-Clause>
+// This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use super::{blcic, tzmsk, multiplicative_inverse};
 
 fn confirm_multiplicative_inverse(a: u64, n: u64) {
