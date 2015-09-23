7e8302a5d7a88d2c73b807740107981a66878fc8
pancake <pancake@nopcode.org>
2015-09-23 02:04:48 +0200

    Add make clean in test

--- radeco/scripts/install-dependencies.sh
+++ radeco/scripts/install-dependencies.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
-echo "[*] Getting radare2" && \
-git clone https://github.com/radare/radare2 && \
-cd radare2 && \
-./sys/install.sh && \
+echo "[*] Getting radare2"
+git clone https://github.com/radare/radare2 || exit 1
+cd radare2 || exit 1
+./sys/install.sh  || exit 1
 echo "[*] radare2 install success"
