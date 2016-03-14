5362d36d5660ae1eb376ce251ab001151aa3f068
Sushant <sushant.dinesh94@gmail.com>
2016-03-15 01:46:39 +0530

    Added coveralls

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
 echo "[*] Getting radare2" && \
+cd ${HOME} && \
 git clone https://github.com/radare/radare2 && \
 cd radare2 && \
 ./sys/user.sh && \
 export PATH=$PATH:${HOME}/bin && \
+cd ${HOME}/radare/radeco-lib && \
 echo "[*] radare2 install success"
