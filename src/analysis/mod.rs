57fad91b0444e71472e3666024fc65486ecb1c24
ZHANG ZHUO <zhangzhuo@sjtu.edu.cn>
2017-09-01 00:07:44 +0800

    Remove old VSA to avoid conflict with new VSA branch in build

--- src/analysis/mod.rs
+++ src/analysis/mod.rs
@@ -8,7 +8,7 @@
 //! Module that implements analysis and optimizations on radeco IR.
 
 #[allow(dead_code)]
-pub mod valueset;
+// pub mod valueset;
 // pub mod propagate;
 pub mod dom;
 pub mod sccp;
