87c72d068d12bfc854559b29afae0212cf06d502
Sushant Dinesh <sushant.dinesh94@gmail.com>
2017-10-01 23:01:58 -0400

    Implements more reference marking

--- src/analysis/mark_refs.rs
+++ src/analysis/mark_refs.rs
@@ -4,6 +4,7 @@
 use petgraph::graph::NodeIndex;
 use middle::ssa::ssastorage::{NodeData, SSAStorage};
 use middle::ssa::ssa_traits::{ValueInfo, ValueType, SSA};
+use middle::ssa::cfg_traits::{CFG};
 use middle::ir::MOpcode;
 
 use std::collections::{HashSet, VecDeque};
@@ -28,104 +29,198 @@ impl ReferenceMarker {
         }
     }
 
+    // Used to return indices nodes which may poetntially be a reference
+    // and need to be checked.
+    // Only propagates to `uses` of `ni`
+    fn propagate_upwards(&self, ni: NodeIndex) -> Vec<NodeIndex> {
+        unimplemented!()
+    }
+
+    // Used to return indices nodes which may poetntially be a reference
+    // and need to be checked.
+    // Only propagates to `args` of `ni`
+    fn propagate_downwards(&self, ni: NodeIndex) -> Vec<NodeIndex> {
+        unimplemented!()
+    }
+
     fn compute_result_arr(&self, ops: &[ValueType]) -> ValueType {
         unimplemented!()
         //ops.fold(ValueType::Unresolved, |acc, &v| { compute_result(&[acc, x]) })
     }
 
     fn resolve_and_mark_subtree(&self, ssa: &mut SSAStorage, subtree: &NodeIndex) -> ValueType {
-        let mut operands = ssa.get_operands(subtree);
-        let rvt = self.compute_result_arr(operands.iter().map(|n| ssa.g[n].get_valueinfo()));
-        match rvt {
-            // All operands are scalar
-            ValueType::Scalar => ValueType::Scalar,
-            // Exactly one operand is a reference
-            ValueType::Reference => ValueType::Reference,
-            // More than one operand has been identified as a reference
-            ValueType::Invalid => ValueType::Invalid,
-            // Unresolved, need more information/analysis to determine which operand is a reference
-            ValueType::Unresolved => {
-                // Need to go deeper!
-                let mut vtys = Vec::new();
-                // Check the opcode to determine the next action.
-                match opc {
-                    // Either operand can be ref
-                    &MOpcode::OpAdd |
-                    &MOpcode::OpAnd |
-                    &MOpcode::OpOr |
-                    &MOpcode::OpXor => {
-                        for op in &operands {
-                            let v = self.resolve_and_mark_subtree(ssa, op);
-                            vtys.push(vtys);
-                        }
-                    }
+        unimplemented!()
+        //let mut operands = ssa.operands_of(*subtree);
+        //let rvt = self.compute_result_arr(operands
+                                          //.iter()
+                                          //.map(|&n| ssa.g[n].get_valueinfo()
+                                                            //.map(|v| v.value_type())
+                                                            //.expect(""))
+                                          //.cloned()
+                                          //.collect::<Vec<_>>()
+                                          //.as_slice());
+        //match rvt {
+            //// All operands are scalar
+            //ValueType::Scalar => ValueType::Scalar,
+            //// Exactly one operand is a reference
+            //ValueType::Reference => ValueType::Reference,
+            //// More than one operand has been identified as a reference
+            //ValueType::Invalid => ValueType::Invalid,
+            //// Unresolved, need more information/analysis to determine which operand is a reference
+            //ValueType::Unresolved => {
+                //// Need to go deeper!
+                //let mut vtys = Vec::new();
+                //// Check the opcode to determine the next action.
+                //let nd  = ssa.g[current].clone();
 
-                    // Second operand can never be ref
-                    &MOpcode::OpSub |
-                    &MOpcode::OpMul |
-                    &MOpcode::OpLsl |
-                    &MOpcode::OpLsr  => { 
-                        self.mark_subtreee_as_scalar(operands[1]);
-                        let v = self.resolve_and_mark_subtree(ssa, operands[0]);
-                        vtys.push(v);
-                    },
+                //match opc {
+                    //// Either operand can be ref
+                    //&MOpcode::OpAdd |
+                    //&MOpcode::OpAnd |
+                    //&MOpcode::OpOr |
+                    //&MOpcode::OpXor => {
+                        //for op in &operands {
+                            //let v = self.resolve_and_mark_subtree(ssa, op);
+                            //vtys.push(v);
+                        //}
+                    //}
 
-                    // Unary operand that should copy current nodes's status
-                    &MOpcode::OpNarrow(u16) |
-                    &MOpcode::OpWiden(u16)  => {
-                        let v = self.resolve_and_mark_subtree(ssa, operands[0]);
-                        vtys.push(v);
-                    },
+                    //// Second operand can never be ref
+                    //&MOpcode::OpSub |
+                    //&MOpcode::OpMul |
+                    //&MOpcode::OpLsl |
+                    //&Opcode::OpRor |
+                    //&Opcode::OpRol |
+                    //&MOpcode::OpLsr  => { 
+                        //self.mark_subtree_as_scalar(ssa, &operands[1]);
+                        //let v = self.resolve_and_mark_subtree(ssa, &operands[0]);
+                        //vtys.push(v);
+                    //},
 
-                    // Special cased to do a section lookup
-                    &MOpcode::OpConst(u64)  => {
-                        // TODO. XXX.
-                        vtys.push(ValueType::Scalar);
-                    },
+                    //// Unary operand that should copy current nodes's status
+                    //&MOpcode::OpNarrow(u16) |
+                    //&MOpcode::OpZeroExt(u16) |
+                    //&MOpcode::OpSignExt(u16)  => {
+                        //let v = self.resolve_and_mark_subtree(ssa, &operands[0]);
+                        //vtys.push(v);
+                    //},
 
-                    // Result can never be ref
-                    &MOpcode::OpDiv |
-                    &MOpcode::OpMod |
-                    &MOpcode::OpNot |
-                    &MOpcode::OpGt |
-                    &MOpcode::OpLt => {
-                        for op in &operands {
-                            self.mark_subtree_as_scalar(ssa, op);
-                        }
-                        vtys.push(ValueType::Scalar);
-                    },
-                }
+                    //// Special cased to do a section lookup
+                    //&MOpcode::OpConst(u64)  => {
+                        //// TODO. XXX.
+                        //vtys.push(ValueType::Scalar);
+                    //},
 
-                // Check if exactly one subtree resolved to be a reference.
-                // TODO.
-                self.compute_result_arr(&vtys)
-            },
-        }
+                    //// Result can never be ref
+                    //&MOpcode::OpDiv |
+                    //&MOpcode::OpMod |
+                    //&MOpcode::OpNot |
+                    //&MOpcode::OpGt |
+                    //&MOpcode::OpLt => {
+                        //for op in &operands {
+                            //self.mark_subtree_as_scalar(ssa, op);
+                        //}
+                        //vtys.push(ValueType::Scalar);
+                    //},
+                //}
+
+                //// Check if exactly one subtree resolved to be a reference.
+                //// TODO.
+                //self.compute_result_arr(&vtys)
+            //},
+        //}
     }
 
     fn mark_subtree_as_scalar(&self, ssa: &mut SSAStorage, subtree: &NodeIndex) {
-        for op in ssa.get_operands(subtree) {
-            if let Some(ref mut nd) = ssa.node_weight_mut(op) {
+        for op in ssa.operands_of(*subtree) {
+            if let Some(ref mut nd) = ssa.g.node_weight_mut(op) {
                 if let Some(ref mut vi) = nd.get_valueinfo_mut() {
                     vi.mark_as_scalar();
                 }
             }
-            self.mark_subtree_as_scalar(ssa, op);
+            self.mark_subtree_as_scalar(ssa, &op);
         }
     }
 
-    pub fn resolve_refs(ssa: &mut SSAStorage) {
+    fn value_type(&self, ssa: &SSAStorage, ni: NodeIndex) -> ValueType {
+        *ssa.g[ni].get_valueinfo().expect("").value_type()
+    }
+
+    fn mark_node(&self, ssa: &mut SSAStorage, ni: NodeIndex, ty: ValueType) {
+        if let Some(ref mut nd) = ssa.g.node_weight_mut(ni) {
+            if let Some(vi) = nd.get_valueinfo_mut() {
+                match vi.value_type() {
+                    &ValueType::Invalid => vi.mark_as_invalid(),
+                    &ValueType::Reference => vi.mark_as_reference(),
+                    &ValueType::Scalar => vi.mark_as_scalar(),
+                    &ValueType::Unresolved => { },
+                }
+            }
+        }
+    }
+
+    // Start from sources where this information can originate.
+    // For marking nodes as references, the source of this information
+    // has to be one of:
+    //   - Address to read/write (memory operations)
+    //   - Target of a indirect CF-transfer (call/jump).
+    //   - Arguments to current function
+    //   - Arguments to call
+    //   - Return from functions
+    //   - Access to stack using rsp/rbp
+    fn mark_trivial_references(&self, ssa: &mut SSAStorage) -> VecDeque<NodeIndex> {
+        let mut wl = VecDeque::new();
+        for block in ssa.blocks() {
+            for e in ssa.exprs_in(block) {
+                // Address to read/write memory must be a reference
+                if let Some(opc) = ssa.opcode(e) {
+                    match opc {
+                        MOpcode::OpStore | MOpcode::OpLoad => {
+                            let operands = ssa.operands_of(e);
+                            self.mark_node(ssa, operands[1], ValueType::Reference);
+                            wl.push_back(operands[1]);
+                        },
+
+                        MOpcode::OpCall => {
+                            // Target of call should be an address
+                            let operands = ssa.operands_of(e);
+                            self.mark_node(ssa, operands[0], ValueType::Reference);
+                            wl.push_back(operands[0]);
+
+                            // Check the arguments to call.
+                            // TODO.
+
+
+                            // Check the return value for a reference
+                            // TODO
+                        },
+
+                        _ => { },
+                    }
+                }
+
+                // For now, only rsp is considered to contain a reference.
+                // Ideally, (For x86) rbp gets marked as a reference automatically due to the
+                // function prologue setting rbp to be equal to rsp.
+                if ssa.registers(e).contains(&"rsp".to_owned()) {
+                    self.mark_node(ssa, e, ValueType::Reference);
+                    wl.push_back(e);
+                }
+
+            }
+        }
+
+        wl
+    }
+
+    pub fn resolve_refs(&mut self, ssa: &mut SSAStorage) {
         let mut wl: VecDeque<NodeIndex> =  VecDeque::new();
         let seen: HashSet<NodeIndex> = HashSet::new();
 
-        // Start from sources where this information can originate.
-        // For marking nodes as references, the source of this information
-        // has to be one of:
-        //   - Address to read/write (memory operations)
-        //   - Target of a indirect CF-transfer (call/jump).
-        //   - Arguments to current function
-        //   - Arguments to  call
-        //   - Return from functions
+
+
+        //   First mark all the trivial references and add them to a worklist.
+        wl = self.mark_trivial_references(ssa);
 
         while !wl.is_empty() {
             let current = wl.pop_front().expect("Cannot be `None`");
@@ -135,7 +230,7 @@ impl ReferenceMarker {
                 NodeData::Op(ref opc, ref vi) => {
                     match opc {
                         &MOpcode::OpStore | &MOpcode::OpLoad => {
-                            let addr = ssa.get_operands(&current)[1];
+                            let addr = ssa.operands_of(current)[1];
                             if let Some(ref mut ind) = ssa.g.node_weight_mut(addr) {
                                 if let Some(ref mut vi) = ind.get_valueinfo_mut() {
                                     vi.mark_as_reference();
