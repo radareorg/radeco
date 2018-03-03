93767c01ec2a30b9167df4275c402579111b5415
kriw <kotarou777775@gmail.com>
2018-03-03 13:13:29 +0900

    Fix call_sites

--- src/analysis/interproc/fixcall.rs
+++ src/analysis/interproc/fixcall.rs
@@ -196,7 +196,7 @@ impl<'a> CallFixer<'a> {
         }
         let call_info: Vec<(LValueRef, Vec<String>)> = {
             let rfn = self.rmod.functions.get(rfn_addr).unwrap();
-            self.preserves_for_call_context(rfn.call_sites().clone())
+            self.preserves_for_call_context(rfn.call_sites(&self.rmod.callgraph).clone())
         };
         radeco_trace!("CallFixer|Call site: {:?}", call_info);
 
@@ -413,47 +413,44 @@ impl<'a> CallFixer<'a> {
 
     // Get callee's node and its preserved registers
     fn preserves_for_call_context(&self, 
-            // call_context: Vec<CallContext<LIdx<B>, LValueRef>>)
-            call_context: Vec<CallContextInfo>)
+            call_context: CallContextInfo)
             -> Vec<(LValueRef, Vec<String>)> 
     {
         let mut result: Vec<(LValueRef, Vec<String>)> = Vec::new(); 
 
-        for con in call_context {
-            // We only analyze the call context which have ssa node.
-            // TODO issue119
-            // if con.ssa_ref.is_none() {
-            //     continue;
-            // }
-            
-            for (caller, callee) in con.map {
-                let mut preserves: Vec<String> = Vec::new();
-                let rfn_opt = self.rmod.functions.get(&con.csite);
-                if let Some(rfn) = rfn_opt {
-                    // Callee is man made function
-                    let mut bindings = rfn.bindings().into_iter();
-                    while let Some(bind) = bindings.next() {
-                        if bind.is_preserved() {
-                            preserves.push(bind.name().to_string());
-                        }
+        // We only analyze the call context which have ssa node.
+        // TODO issue119
+        // if con.ssa_ref.is_none() {
+        //     continue;
+        // }
+
+        for (caller, callee) in call_context.map {
+            let mut preserves: Vec<String> = Vec::new();
+            let rfn_opt = self.rmod.functions.get(&call_context.csite);
+            if let Some(rfn) = rfn_opt {
+                // Callee is man made function
+                let mut bindings = rfn.bindings().into_iter();
+                while let Some(bind) = bindings.next() {
+                    if bind.is_preserved() {
+                        preserves.push(bind.name().to_string());
                     }
-                    //TODO issue119
-                    // let ssa_ref = con.ssa_ref.unwrap_or_else(|| {
-                    //     radeco_err!("con.ssa_ref == None");
-                    //     NodeIndex::end()
-                    // });
-                    // result.push((ssa_ref, preserves));
-                } else {
-                    // Callee is library function
-                    //TODO issue119
-                    unimplemented!()
-                    // let ssa_ref = con.ssa_ref.unwrap_or_else(|| {
-                    //     radeco_err!("con.ssa_ref == None");
-                    //     NodeIndex::end()
-                    // });
-                    // let bp_name = vec![self.sp_name.clone().unwrap_or(String::new())];
-                    // result.push((ssa_ref, bp_name));
                 }
+                //TODO issue119
+                // let ssa_ref = con.ssa_ref.unwrap_or_else(|| {
+                //     radeco_err!("con.ssa_ref == None");
+                //     NodeIndex::end()
+                // });
+                // result.push((ssa_ref, preserves));
+            } else {
+                // Callee is library function
+                //TODO issue119
+                unimplemented!()
+                // let ssa_ref = con.ssa_ref.unwrap_or_else(|| {
+                //     radeco_err!("con.ssa_ref == None");
+                //     NodeIndex::end()
+                // });
+                // let bp_name = vec![self.sp_name.clone().unwrap_or(String::new())];
+                // result.push((ssa_ref, bp_name));
             }
         }
 
