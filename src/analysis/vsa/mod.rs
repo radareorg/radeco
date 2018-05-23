9090571251fa7f4cefaa6f74ca00ef2c0484d565
ZhangZhuoSJTU <zhangzhuo@sjtu.edu.cn>
2018-05-23 16:02:26 +0800

    Set up for strided interval

---
+++ src/analysis/vsa/mod.rs
@@ -0,0 +1,13 @@
+// Copyright (c) 2015, The Radare Project. All rights reserved.
+// See the COPYING file at the top-level directory of this distribution.
+// Licensed under the BSD 3-Clause License:
+// <http://opensource.org/licenses/BSD-3-Clause>
+// This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! Module that implements value set analysis on radeco IR.
+//! More information are available from Gogul Balakrishnan's Ph.D. Thesis.
+//! Please refer here:
+//!     * https://research.cs.wisc.edu/wpis/papers/balakrishnan_thesis.pdf
+
+pub mod strided_interval;
