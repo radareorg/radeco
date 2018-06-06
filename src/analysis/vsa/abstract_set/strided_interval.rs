47f4a09d08f66c2d20d77b9cce33bb8401204ec5
ZhangZhuoSJTU <zhangzhuo@sjtu.edu.cn>
2018-06-06 11:54:12 +0800

    Update copyright

--- src/analysis/vsa/abstract_set/strided_interval.rs
+++ src/analysis/vsa/abstract_set/strided_interval.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, The Radare Project. All rights reserved.
+// Copyright (c) 2018, The Radare Project. All rights reserved.
 // See the COPYING file at the top-level directory of this distribution.
 // Licensed under the BSD 3-Clause License:
 // <http://opensource.org/licenses/BSD-3-Clause>
