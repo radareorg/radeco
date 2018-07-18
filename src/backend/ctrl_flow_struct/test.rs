4971b2b3f4f46ac3425607e70ca808928ba64e76
HMPerson1 <hmperson1@gmail.com>
2018-07-18 16:00:34 -0400

    [ctrl_flow_struct] Cleanup tests

--- src/backend/ctrl_flow_struct/test.rs
+++ src/backend/ctrl_flow_struct/test.rs
@@ -56,46 +56,56 @@ fn ast_nmg_example() {
     let exit = graph.add_node(empty_node());
 
     #[allow(non_snake_case)]
-    let c_A = cond_s(&cctx, "A");
-    let c_c1 = cond_s(&cctx, "c1");
-    let c_c2 = cond_s(&cctx, "c2");
-    let c_c3 = cond_s(&cctx, "c3");
-    let c_b1 = cond_s(&cctx, "b1");
-    let c_b2 = cond_s(&cctx, "b2");
-    let c_d1 = cond_s(&cctx, "d1");
-    let c_d2 = cond_s(&cctx, "d2");
-    let c_d3 = cond_s(&cctx, "d3");
+    let c_A = cond_s(cctx, "A");
+    let c_c1 = cond_s(cctx, "c1");
+    let c_c2 = cond_s(cctx, "c2");
+    let c_c3 = cond_s(cctx, "c3");
+    let c_b1 = cond_s(cctx, "b1");
+    let c_b2 = cond_s(cctx, "b2");
+    let c_d1 = cond_s(cctx, "d1");
+    let c_d2 = cond_s(cctx, "d2");
+    let c_d3 = cond_s(cctx, "d3");
+    #[allow(non_snake_case)]
+    let nc_A = cctx.mk_not(c_A);
+    let nc_c1 = cctx.mk_not(c_c1);
+    let nc_c2 = cctx.mk_not(c_c2);
+    let nc_c3 = cctx.mk_not(c_c3);
+    let nc_b1 = cctx.mk_not(c_b1);
+    let nc_b2 = cctx.mk_not(c_b2);
+    let nc_d1 = cctx.mk_not(c_d1);
+    let nc_d2 = cctx.mk_not(c_d2);
+    let nc_d3 = cctx.mk_not(c_d3);
 
     graph.add_edge(entry, c1, Some(c_A));
-    graph.add_edge(entry, b1, neg_c(&cctx, c_A));
+    graph.add_edge(entry, b1, Some(nc_A));
     graph.add_edge(n9, exit, None);
     // R1
     graph.add_edge(c1, n1, Some(c_c1));
     graph.add_edge(n1, c1, None);
-    graph.add_edge(c1, c2, neg_c(&cctx, c_c1));
+    graph.add_edge(c1, c2, Some(nc_c1));
     graph.add_edge(c2, n2, Some(c_c2));
     graph.add_edge(n2, n9, None);
-    graph.add_edge(c2, n3, neg_c(&cctx, c_c2));
+    graph.add_edge(c2, n3, Some(nc_c2));
     graph.add_edge(n3, c3, None);
     graph.add_edge(c3, c1, Some(c_c3));
-    graph.add_edge(c3, n9, neg_c(&cctx, c_c3));
+    graph.add_edge(c3, n9, Some(nc_c3));
     // R2
     graph.add_edge(b1, b2, Some(c_b1));
     graph.add_edge(b2, n6, Some(c_b2));
     graph.add_edge(n6, n7, None);
     graph.add_edge(n7, d1, None);
-    graph.add_edge(b2, n5, neg_c(&cctx, c_b2));
+    graph.add_edge(b2, n5, Some(nc_b2));
     graph.add_edge(n5, n7, None);
-    graph.add_edge(b1, n4, neg_c(&cctx, c_b1));
+    graph.add_edge(b1, n4, Some(nc_b1));
     graph.add_edge(n4, n5, None);
     // R3
     graph.add_edge(d1, d3, Some(c_d1));
     graph.add_edge(d3, n8, Some(c_d3));
     graph.add_edge(n8, d1, None);
-    graph.add_edge(d3, n9, neg_c(&cctx, c_d3));
-    graph.add_edge(d1, d2, neg_c(&cctx, c_d1));
+    graph.add_edge(d3, n9, Some(nc_d3));
+    graph.add_edge(d1, d2, Some(nc_d1));
     graph.add_edge(d2, n8, Some(c_d2));
-    graph.add_edge(d2, n9, neg_c(&cctx, c_d2));
+    graph.add_edge(d2, n9, Some(nc_d2));
 
     let actx = StringAst::default();
     let cfg = ControlFlowGraph {
@@ -142,7 +152,7 @@ fn ast_nmg_example() {
                     BasicBlock("n7".to_owned()),
                     Loop(
                         LoopType::PreChecked(
-                            cctx.mk_or(cctx.mk_and(c_d1, c_d3), cctx.mk_and(cctx.mk_not(c_d1), c_d2)),
+                            cctx.mk_or(cctx.mk_and(c_d1, c_d3), cctx.mk_and(nc_d1, c_d2)),
                         ),
                         Box::new(BasicBlock("n8".to_owned())),
                     ),
@@ -169,9 +179,9 @@ fn ast_nmg_r1() {
     let n3 = graph.add_node(node("n3"));
     let exit = graph.add_node(empty_node());
 
-    let c_c1 = cond_s(&cctx, "c1");
-    let c_c2 = cond_s(&cctx, "c2");
-    let c_c3 = cond_s(&cctx, "c3");
+    let c_c1 = cond_s(cctx, "c1");
+    let c_c2 = cond_s(cctx, "c2");
+    let c_c3 = cond_s(cctx, "c3");
     let nc_c1 = cctx.mk_not(c_c1);
     let nc_c2 = cctx.mk_not(c_c2);
     let nc_c3 = cctx.mk_not(c_c3);
@@ -233,15 +243,17 @@ fn ast_nmg_r2() {
     let n7 = graph.add_node(node("n7"));
     let exit = graph.add_node(empty_node());
 
-    let c_b1 = cond_s(&cctx, "b1");
-    let c_b2 = cond_s(&cctx, "b2");
+    let c_b1 = cond_s(cctx, "b1");
+    let c_b2 = cond_s(cctx, "b2");
+    let nc_b1 = cctx.mk_not(c_b1);
+    let nc_b2 = cctx.mk_not(c_b2);
 
     graph.add_edge(b1, b2, Some(c_b1));
     graph.add_edge(b2, n6, Some(c_b2));
     graph.add_edge(n6, n7, None);
-    graph.add_edge(b2, n5, neg_c(&cctx, c_b2));
+    graph.add_edge(b2, n5, Some(nc_b2));
     graph.add_edge(n5, n7, None);
-    graph.add_edge(b1, n4, neg_c(&cctx, c_b1));
+    graph.add_edge(b1, n4, Some(nc_b1));
     graph.add_edge(n4, n5, None);
     graph.add_edge(n7, exit, None);
 
@@ -288,9 +300,9 @@ fn ast_nmg_r3() {
     let n8 = graph.add_node(node("n8"));
     let exit = graph.add_node(empty_node());
 
-    let c_d1 = cond_s(&cctx, "d1");
-    let c_d2 = cond_s(&cctx, "d2");
-    let c_d3 = cond_s(&cctx, "d3");
+    let c_d1 = cond_s(cctx, "d1");
+    let c_d2 = cond_s(cctx, "d2");
+    let c_d3 = cond_s(cctx, "d3");
     let nc_d1 = cctx.mk_not(c_d1);
     let nc_d2 = cctx.mk_not(c_d2);
     let nc_d3 = cctx.mk_not(c_d3);
@@ -361,28 +373,35 @@ fn ast_switchy() {
     let n3 = graph.add_node(node("n3"));
     let exit = graph.add_node(empty_node());
 
-    let c_c1 = cond_s(&cctx, "n == 7");
-    let c_c2 = cond_s(&cctx, "n <= 7");
-    let c_c3 = cond_s(&cctx, "n == 88");
-    let c_c4 = cond_s(&cctx, "n == 1");
-    let c_c5 = cond_s(&cctx, "n == 98");
-    let c_c6 = cond_s(&cctx, "n != 4");
-    let c_c7 = cond_s(&cctx, "n == 34");
+    let c_c1 = cond_s(cctx, "n == 7");
+    let c_c2 = cond_s(cctx, "n <= 7");
+    let c_c3 = cond_s(cctx, "n == 88");
+    let c_c4 = cond_s(cctx, "n == 1");
+    let c_c5 = cond_s(cctx, "n == 98");
+    let c_c6 = cond_s(cctx, "n != 4");
+    let c_c7 = cond_s(cctx, "n == 34");
+    let nc_c1 = cctx.mk_not(c_c1);
+    let nc_c2 = cctx.mk_not(c_c2);
+    let nc_c3 = cctx.mk_not(c_c3);
+    let nc_c4 = cctx.mk_not(c_c4);
+    let nc_c5 = cctx.mk_not(c_c5);
+    let nc_c6 = cctx.mk_not(c_c6);
+    let nc_c7 = cctx.mk_not(c_c7);
 
     graph.add_edge(c1, n1, Some(c_c1));
-    graph.add_edge(c1, c2, Some(cctx.mk_not(c_c1)));
+    graph.add_edge(c1, c2, Some(nc_c1));
     graph.add_edge(c2, c4, Some(c_c2));
-    graph.add_edge(c2, c3, Some(cctx.mk_not(c_c2)));
+    graph.add_edge(c2, c3, Some(nc_c2));
     graph.add_edge(c3, n2, Some(c_c3));
-    graph.add_edge(c3, c5, Some(cctx.mk_not(c_c3)));
+    graph.add_edge(c3, c5, Some(nc_c3));
     graph.add_edge(c4, n1, Some(c_c4));
-    graph.add_edge(c4, c6, Some(cctx.mk_not(c_c4)));
+    graph.add_edge(c4, c6, Some(nc_c4));
     graph.add_edge(c5, n1, Some(c_c5));
-    graph.add_edge(c5, c7, Some(cctx.mk_not(c_c5)));
+    graph.add_edge(c5, c7, Some(nc_c5));
     graph.add_edge(c6, n3, Some(c_c6));
-    graph.add_edge(c6, n1, Some(cctx.mk_not(c_c6)));
+    graph.add_edge(c6, n1, Some(nc_c6));
     graph.add_edge(c7, n2, Some(c_c7));
-    graph.add_edge(c7, n3, Some(cctx.mk_not(c_c7)));
+    graph.add_edge(c7, n3, Some(nc_c7));
     graph.add_edge(n1, exit, None);
     graph.add_edge(n2, exit, None);
     graph.add_edge(n3, exit, None);
@@ -429,11 +448,11 @@ fn ast_ifelse_cascade() {
     let n3 = graph.add_node(node("n3"));
     let exit = graph.add_node(empty_node());
 
-    let c_c1 = cond_s(&cctx, "c1");
-    let c_c2 = cond_s(&cctx, "c2");
-    let c_c3 = cond_s(&cctx, "c3");
-    let c_c4 = cond_s(&cctx, "c4");
-    let c_c5 = cond_s(&cctx, "c5");
+    let c_c1 = cond_s(cctx, "c1");
+    let c_c2 = cond_s(cctx, "c2");
+    let c_c3 = cond_s(cctx, "c3");
+    let c_c4 = cond_s(cctx, "c4");
+    let c_c5 = cond_s(cctx, "c5");
     let nc_c1 = cctx.mk_not(c_c1);
     let nc_c2 = cctx.mk_not(c_c2);
     let nc_c3 = cctx.mk_not(c_c3);
@@ -496,7 +515,7 @@ fn ast_while() {
     let n = graph.add_node(node("n"));
     let exit = graph.add_node(empty_node());
 
-    let c_c = cond_s(&cctx, "c");
+    let c_c = cond_s(cctx, "c");
     let nc_c = cctx.mk_not(c_c);
 
     graph.add_edge(entry, c, None);
@@ -541,7 +560,7 @@ fn ast_do_while() {
     let n = graph.add_node(node("n"));
     let exit = graph.add_node(empty_node());
 
-    let c_c = cond_s(&cctx, "c");
+    let c_c = cond_s(cctx, "c");
     let nc_c = cctx.mk_not(c_c);
 
     graph.add_edge(entry, n, None);
@@ -625,10 +644,10 @@ fn ast_complex_while_and() {
     let n1 = graph.add_node(node("n1"));
     let exit = graph.add_node(empty_node());
 
-    let c_c1 = cond_s(&cctx, "c1");
-    let c_c2 = cond_s(&cctx, "c2");
-    let c_c3 = cond_s(&cctx, "c3");
-    let c_c4 = cond_s(&cctx, "c4");
+    let c_c1 = cond_s(cctx, "c1");
+    let c_c2 = cond_s(cctx, "c2");
+    let c_c3 = cond_s(cctx, "c3");
+    let c_c4 = cond_s(cctx, "c4");
     let nc_c1 = cctx.mk_not(c_c1);
     let nc_c2 = cctx.mk_not(c_c2);
     let nc_c3 = cctx.mk_not(c_c3);
@@ -685,10 +704,10 @@ fn ast_complex_while_or() {
     let n1 = graph.add_node(node("n1"));
     let exit = graph.add_node(empty_node());
 
-    let c_c1 = cond_s(&cctx, "c1");
-    let c_c2 = cond_s(&cctx, "c2");
-    let c_c3 = cond_s(&cctx, "c3");
-    let c_c4 = cond_s(&cctx, "c4");
+    let c_c1 = cond_s(cctx, "c1");
+    let c_c2 = cond_s(cctx, "c2");
+    let c_c3 = cond_s(cctx, "c3");
+    let c_c4 = cond_s(cctx, "c4");
     let nc_c1 = cctx.mk_not(c_c1);
     let nc_c2 = cctx.mk_not(c_c2);
     let nc_c3 = cctx.mk_not(c_c3);
@@ -738,44 +757,51 @@ fn abnormal_entries() {
     let n3 = graph.add_node(cnode());
     let n4 = graph.add_node(cnode());
     let n5 = graph.add_node(cnode());
-    let f = graph.add_node(node("f"));
     let l1 = graph.add_node(cnode());
     let l2 = graph.add_node(node("l2"));
     let l3 = graph.add_node(node("l3"));
+    let exit = graph.add_node(empty_node());
 
-    let c_e1 = cond_s(&cctx, "e1");
-    let c_n1 = cond_s(&cctx, "n1");
-    let c_n2 = cond_s(&cctx, "n2");
-    let c_n3 = cond_s(&cctx, "n3");
-    let c_n4 = cond_s(&cctx, "n4");
-    let c_n5 = cond_s(&cctx, "n5");
-    let c_l1 = cond_s(&cctx, "l1");
+    let c_e1 = cond_s(cctx, "e1");
+    let c_n1 = cond_s(cctx, "n1");
+    let c_n2 = cond_s(cctx, "n2");
+    let c_n3 = cond_s(cctx, "n3");
+    let c_n4 = cond_s(cctx, "n4");
+    let c_n5 = cond_s(cctx, "n5");
+    let c_l1 = cond_s(cctx, "l1");
+    let nc_e1 = cctx.mk_not(c_e1);
+    let nc_n1 = cctx.mk_not(c_n1);
+    let nc_n2 = cctx.mk_not(c_n2);
+    let nc_n3 = cctx.mk_not(c_n3);
+    let nc_n4 = cctx.mk_not(c_n4);
+    let nc_n5 = cctx.mk_not(c_n5);
+    let nc_l1 = cctx.mk_not(c_l1);
 
     graph.add_edge(entry, l1, Some(c_e1));
-    graph.add_edge(entry, n1, neg_c(&cctx, c_e1));
+    graph.add_edge(entry, n1, Some(nc_e1));
     graph.add_edge(n1, n2, Some(c_n1));
     graph.add_edge(n2, n3, Some(c_n2));
     graph.add_edge(n3, n4, Some(c_n3));
     graph.add_edge(n4, n5, Some(c_n4));
-    graph.add_edge(n5, f, Some(c_n5));
+    graph.add_edge(n5, exit, Some(c_n5));
     // loop
     graph.add_edge(l1, l2, Some(c_l1));
     graph.add_edge(l2, l3, None);
     graph.add_edge(l3, l1, None);
     // loop exit
-    graph.add_edge(l1, f, neg_c(&cctx, c_l1));
+    graph.add_edge(l1, exit, Some(nc_l1));
     // abnormal entries
-    graph.add_edge(n1, l1, neg_c(&cctx, c_n1));
-    graph.add_edge(n2, l2, neg_c(&cctx, c_n2));
-    graph.add_edge(n3, l3, neg_c(&cctx, c_n3));
-    graph.add_edge(n4, l2, neg_c(&cctx, c_n4));
-    graph.add_edge(n5, l2, neg_c(&cctx, c_n5));
+    graph.add_edge(n1, l1, Some(nc_n1));
+    graph.add_edge(n2, l2, Some(nc_n2));
+    graph.add_edge(n3, l3, Some(nc_n3));
+    graph.add_edge(n4, l2, Some(nc_n4));
+    graph.add_edge(n5, l2, Some(nc_n5));
 
     let actx = StringAst::default();
     let cfg = ControlFlowGraph {
         graph,
         entry,
-        exit: f,
+        exit,
         cctx,
         actx,
     };
@@ -795,27 +821,33 @@ fn abnormal_exits() {
     let n3 = graph.add_node(node("n3"));
     let n4 = graph.add_node(node("n4"));
     let n5 = graph.add_node(node("n5"));
-    let f = graph.add_node(node("f"));
     let l1 = graph.add_node(cnode());
     let l2 = graph.add_node(cnode());
     let l3 = graph.add_node(cnode());
     let l4 = graph.add_node(cnode());
     let l5 = graph.add_node(cnode());
+    let exit = graph.add_node(empty_node());
 
-    let c_e1 = cond_s(&cctx, "e1");
-    let c_l1 = cond_s(&cctx, "l1");
-    let c_l2 = cond_s(&cctx, "l2");
-    let c_l3 = cond_s(&cctx, "l3");
-    let c_l4 = cond_s(&cctx, "l4");
-    let c_l5 = cond_s(&cctx, "l5");
+    let c_e1 = cond_s(cctx, "e1");
+    let c_l1 = cond_s(cctx, "l1");
+    let c_l2 = cond_s(cctx, "l2");
+    let c_l3 = cond_s(cctx, "l3");
+    let c_l4 = cond_s(cctx, "l4");
+    let c_l5 = cond_s(cctx, "l5");
+    let nc_e1 = cctx.mk_not(c_e1);
+    let nc_l1 = cctx.mk_not(c_l1);
+    let nc_l2 = cctx.mk_not(c_l2);
+    let nc_l3 = cctx.mk_not(c_l3);
+    let nc_l4 = cctx.mk_not(c_l4);
+    let nc_l5 = cctx.mk_not(c_l5);
 
     graph.add_edge(entry, l1, Some(c_e1));
-    graph.add_edge(entry, n1, neg_c(&cctx, c_e1));
+    graph.add_edge(entry, n1, Some(nc_e1));
     graph.add_edge(n1, n2, None);
     graph.add_edge(n2, n3, None);
     graph.add_edge(n3, n4, None);
     graph.add_edge(n4, n5, None);
-    graph.add_edge(n5, f, None);
+    graph.add_edge(n5, exit, None);
     // loop
     graph.add_edge(l1, l2, Some(c_l1));
     graph.add_edge(l2, l3, Some(c_l2));
@@ -823,18 +855,18 @@ fn abnormal_exits() {
     graph.add_edge(l4, l5, Some(c_l4));
     graph.add_edge(l5, l1, Some(c_l5));
     // loop exit
-    graph.add_edge(l1, f, neg_c(&cctx, c_l1));
-    graph.add_edge(l4, f, neg_c(&cctx, c_l4));
+    graph.add_edge(l1, exit, Some(nc_l1));
+    graph.add_edge(l4, exit, Some(nc_l4));
     // abnormal exits
-    graph.add_edge(l2, n2, neg_c(&cctx, c_l2));
-    graph.add_edge(l3, n2, neg_c(&cctx, c_l3));
-    graph.add_edge(l5, n5, neg_c(&cctx, c_l5));
+    graph.add_edge(l2, n2, Some(nc_l2));
+    graph.add_edge(l3, n2, Some(nc_l3));
+    graph.add_edge(l5, n5, Some(nc_l5));
 
     let actx = StringAst::default();
     let cfg = ControlFlowGraph {
         graph,
         entry,
-        exit: f,
+        exit,
         cctx,
         actx,
     };
@@ -842,15 +874,8 @@ fn abnormal_exits() {
     println!("{:#?}", ast);
 }
 
-fn cond_s<'cd>(cctx: &condition::Context<'cd, String>, c: &str) -> Condition<'cd, StringAst> {
+fn cond_s<'cd>(cctx: condition::Context<'cd, String>, c: &str) -> Condition<'cd, StringAst> {
     cctx.mk_var(c.to_owned())
-}
-
-fn neg_c<'cd>(
-    cctx: &condition::Context<'cd, String>,
-    c: Condition<'cd, StringAst>,
-) -> Option<Condition<'cd, StringAst>> {
-    Some(cctx.mk_not(c))
 }
 
 fn node(n: &str) -> CfgNode<'static, StringAst> {
