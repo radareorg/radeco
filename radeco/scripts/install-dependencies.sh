34d7920b6ac82757d417d12de12f1d905b3225e9
Anton Kochkov <xvilka@gmail.com>
2018-08-28 12:23:46 +0800

    Try more updated GCC

--- radeco/scripts/install-dependencies.sh
+++ radeco/scripts/install-dependencies.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# This is because Travis CI uses very old GCC version
+# which still not have C99 as a default.
+# .travis.yml has installation instructions for gcc-7
+# Thus we enable it as a default compiler for dependencies
+export CC=gcc-7
+export CXX=g++-7
 echo "[*] Getting radare2"
 git clone https://github.com/radare/radare2 || exit 1
 cd radare2 || exit 1
