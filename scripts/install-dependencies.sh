fc7c09c17c15bbfbf021130c30d9d317224545a0
Anton Kochkov <anton.kochkov@gmail.com>
2019-06-18 15:48:43 +0800

    Radeco - integrate into the Travis build

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh
 
-echo "[*] Getting radare2" && \
-cd ${HOME} && \
-git clone https://github.com/radare/radare2 && \
-cd radare2 && \
-./sys/user.sh && \
-export PATH=$PATH:${HOME}/bin && \
-cd ${HOME}/build/radare/radeco-lib && \
+# This is because Travis CI uses very old GCC version
+# which still not have C99 as a default.
+# .travis.yml has installation instructions for gcc-7
+# Thus we enable it as a default compiler for dependencies
+export CC=gcc-7
+export CXX=g++-7
+
+echo "[*] Getting radare2"
+git clone https://github.com/radare/radare2 || exit 1
+cd radare2 || exit 1
+./sys/install.sh || exit 1
 echo "[*] radare2 install success"
